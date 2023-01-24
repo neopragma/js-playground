40451a0a252ad2875d4f61c5e3a742015a163ac5
neopragma <davenicolette@gmail.com>
2023-01-24 13:51:14 -0700

    Starter code for strings

---
+++ strings/test/strings.test.js
@@ -0,0 +1,9 @@
+import 'mocha';
+import { expect } from 'chai';
+expect();
+
+describe('Strings', () => {
+    it('does something with a string', () => {
+        expect(foo).to.equal('bar');
+    })
+})
